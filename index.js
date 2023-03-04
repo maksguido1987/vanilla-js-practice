ef40cbcd5c53e8de31fea58560cdd8acf101dfbc
Maksim Guido <maksguido@gmail.com>
2023-03-04 11:19:02 +0300

    feat: create footer

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-const modal = $.modal()
+const modal = $.modal({
+  title: "Title",
+  closable: true,
+  content: `<h4>Hello</h4><p>lorem lorem lorem lorem</p>`,
+  maxWidth: "500px",
+  footerButtons: [
+    {
+      text: "Cancel",
+      type: "secondary",
+      handler: function () {
+        modal.close();
+      },
+    },
+    {
+      text: "Ok",
+      type: "primary",
+      handler: function () {
+        modal.close();
+      },
+    },
+  ],
+});
--- plugins/modal.js
+++ plugins/modal.js
@@ -1,57 +1,85 @@
+Element.prototype.appendAfter = function (element) {
+  element.parentNode.insertBefore(this, element.nextSibling);
+};
+
+function _createModalFooter(buttons = []) {
+  if (buttons.length === 0) {
+    return document.createElement("div");
+  }
+  const wrap = document.createElement("div");
+  wrap.classList.add("modal-footer");
+
+  buttons.forEach((btn) => {
+    const $btn = document.createElement("button");
+    $btn.textContent = btn.text;
+    $btn.classList.add("btn");
+    $btn.classList.add(`btn-${btn.type || "secondary"}`);
+    $btn.onclick = btn.handler;
+
+    wrap.appendChild($btn);
+  });
+
+  return wrap;
+}
+
 function _createModal(options) {
+  const DEFAULT_MAX_WIDTH = "600px";
+
   const modal = document.createElement("div");
   modal.classList.add("vmodal");
   modal.insertAdjacentHTML(
     "afterbegin",
     `
-    <div class="modal-overlay">
-      <div class="modal-window">
+    <div class="modal-overlay" data-close="true">
+      <div class="modal-window" style="max-width: ${
+        options.maxWidth || DEFAULT_MAX_WIDTH
+      }">
         <div class="modal-header">
-          <h5 class="modal-title">Modal title</h5>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
+          <h5 class="modal-title">${options.title || "Title"}</h5>
+          ${
+            options.closable
+              ? `<button
+                    data-close="true"
+                    type="button"
+                    class="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>`
+              : ""
+          }
         </div>
-        <div class="modal-body"></div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary">Cancel</button>
-          <button type="button" class="btn btn-primary">Ok</button>
-        </div>
+        <div class="modal-body" data-body>${options.content || ""}</div>
       </div>
     </div>
     `
   );
+  const footer = _createModalFooter(options.footerButtons);
+  footer.appendAfter(modal.querySelector("[data-body]"));
+
   document.body.append(modal);
 
   return modal;
 }
 
 /*
-* title: string
-* closable: boolean
-* content: string
-* width: string ('400px')
-* destroy(): void
-* Окно должно закрываться
-* --------------
-* setContent(html: string): void | PUBLIC
-* onClose(): void
-* onOpen(): void
-* beforeClose(): boolean
-* --------------
-* animate.css
-* */
+ * onClose(): void
+ * onOpen(): void
+ * beforeClose(): boolean
+ * --------------
+ * animate.css
+ * */
 
 $.modal = function (options) {
   const ANIMATION_TIME = 300;
   const $modal = _createModal(options);
   let closing = false;
+  let isDestroy = false;
 
-  return {
+  const modalActions = {
     open() {
+      if (isDestroy) {
+        return;
+      }
       !closing && $modal.classList.add("open");
     },
     close() {
@@ -63,6 +91,24 @@ $.modal = function (options) {
         closing = false;
       }, ANIMATION_TIME);
     },
-    destroy() {},
   };
+
+  const listener = (event) => {
+    if (event.target.dataset.close) {
+      modalActions.close();
+    }
+  };
+
+  $modal.addEventListener("click", listener);
+
+  return Object.assign(modalActions, {
+    destroy() {
+      $modal.parentNode.removeChild($modal);
+      $modal.removeEventListener("click", listener);
+      isDestroy = true;
+    },
+    setContent(html) {
+      $modal.querySelector("[data-body]").innerHTML = html;
+    },
+  });
 };
